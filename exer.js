9d3e16d9ea9522a441c2ca339d6cdde200ef57d6
NISRINE204 <nissrinlazmi457@gmail.com>
2024-11-21 16:57:18 +0100

    Add files via upload

---
+++ exer.js
@@ -0,0 +1,11 @@
+for(let i=1;i<=19;i++){
+    if(i % 3==1 ){
+        console.log(i)
+    }
+}
+
+let fruit=["banana","fraise","poivre"]
+fruit.forEach(function(fruit){
+    console.log(fruit.toUpperCase(fruit))
+})
+    
